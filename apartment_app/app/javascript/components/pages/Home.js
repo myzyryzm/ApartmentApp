0fca717431c0b463befc642af332a860a6d19c96
myzyryzm <ryanyoung3e@yahoo.com>
2019-11-30 12:30:56 -0800

    11/30-12:30 commit

--- apartment_app/app/javascript/components/pages/Home.js
+++ apartment_app/app/javascript/components/pages/Home.js
@@ -1,19 +1,10 @@
 import React from "react"
-import {Link} from "react-router-dom"
 
 export default class Home extends React.Component {
     render () {
-        const {apartments} = this.props
-        let apt = apartments.map(apt => {
-            return <li key = {apt.id}>{apt.street_a} {apt.city}
-            <div><Link to={`/apartment/${apt.id}`}>Show Details</Link></div>
-            </li>
-        })
         return (
-          <div className = "layout">
-            <ul>
-              {apt}
-            </ul>
+          <div >
+            <h1 className = "whiteText">Are you a big baller?  Only big ballers allowed for the flyest Apartments left of your left side.  NO FLAT EARTHERS ALLOWED!!!!</h1>
           </div>
         );
     }
